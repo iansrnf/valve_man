1a21727ad1a6516d2f13e4e65cdd4f682d2038cc
IANSR_GSCWD <ianspencer0904@gmail.com>
2024-08-15 08:31:16 +0800

    save data to database

--- src/iot-valve/valve.websocket.ts
+++ src/iot-valve/valve.websocket.ts
@@ -1,10 +1,34 @@
+import { InjectRepository } from '@nestjs/typeorm';
 import { MessageBody, SubscribeMessage, WebSocketGateway } from '@nestjs/websockets';
+import { IotValve } from './entities/iot-valve.entity';
+import { Repository } from 'typeorm';
 
 @WebSocketGateway()
 export class ValveWebSocket {
+  constructor(@InjectRepository(IotValve) private readonly valveRepository: Repository<IotValve>) {}
   @SubscribeMessage('newMessage')
-  onNewMessage(@MessageBody() body: any) {
-    console.log(body);
-    return body;
+  async onNewMessage(@MessageBody() input: IotValve) {
+    console.log(input);
+    const valveList = await this.valveRepository.find();
+    if (valveList) {
+      if (valveList.length == 0) {
+        //save first angle
+        const myValve = new IotValve();
+        Object.assign(myValve, input);
+        await this.valveRepository.save(myValve);
+      } else {
+        let myValve = new IotValve();
+        valveList.forEach((e) => {
+          myValve = e;
+        });
+        Object.assign(myValve, input);
+        await this.valveRepository.save(myValve);
+      }
+    } else {
+      const myValve = new IotValve();
+      Object.assign(myValve, input);
+      await this.valveRepository.save(myValve);
+    }
+    return input;
   }
 }
